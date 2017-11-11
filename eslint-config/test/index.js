12f7f3bce3b83ba64f00c188f7595f6308262403
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2017-11-11 14:41:16 -0500

    add placeholder tests

---
+++ eslint-config/test/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file Tests for ESLint rules
+ *
+ * @author Ty-Lucas Kelley <ty@greylocklabs.com> (https://greylocklabs.com)
+ * @copyright Copyright (c) 2017 Greylock Labs. See LICENSE file for details.
+ */
+
+import test from 'ava';
+
+test('Placeholder test', (t) => {
+    t.pass();
+});
